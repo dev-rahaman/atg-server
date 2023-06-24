baf5f64cca7e28c5fa7986151740b39c78a102dd
rs rahaman <jesminakter.rahaman@gmail.com>
2023-06-24 23:08:22 +0600

    fdjsl

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGODB_USER_KEY}:${process.env.MONGODB_PASS_KEY}@cluster0.bk91ias.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,6 +39,7 @@ const articlesCollection = client.db("atg").collection("articles");
 const educationsCollection = client.db("atg").collection("educations");
 const eventsCollection = client.db("atg").collection("events");
 const jobsCollection = client.db("atg").collection("jobs");
+const usersCollection = client.db("atg").collection("users");
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -121,6 +122,88 @@ function shuffleArray(array) {
   }
   return shuffled;
 }
+
+// Registration
+app.post("/users", async (req, res) => {
+  const users = req.body;
+  const result = usersCollection.insertOne(users);
+  res.send(result);
+});
+
+// Login
+
+// like
+app.patch("/like/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+
+  try {
+    const article = await articlesCollection.findOne(filter);
+    let likeCount = parseInt(article.like);
+    likeCount = isNaN(likeCount) ? 0 : likeCount;
+
+    const updateDoc = { $set: { like: likeCount + 1 } };
+    const result = await articlesCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating the like count.");
+  }
+});
+
+app.patch("/event-like/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+
+  try {
+    const event = await eventsCollection.findOne(filter);
+    let likeCount = parseInt(event.like);
+    likeCount = isNaN(likeCount) ? 0 : likeCount;
+
+    const updateDoc = { $set: { like: likeCount + 1 } };
+    const result = await eventsCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating the like count.");
+  }
+});
+
+app.patch("/job-like/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+
+  try {
+    const job = await jobsCollection.findOne(filter);
+    let likeCount = parseInt(job.like);
+    likeCount = isNaN(likeCount) ? 0 : likeCount;
+
+    const updateDoc = { $set: { like: likeCount + 1 } };
+    const result = await jobsCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating the like count.");
+  }
+});
+
+app.patch("/education-like/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+
+  try {
+    const education = await educationsCollection.findOne(filter);
+    let likeCount = parseInt(education.like);
+    likeCount = isNaN(likeCount) ? 0 : likeCount;
+
+    const updateDoc = { $set: { like: likeCount + 1 } };
+    const result = await educationsCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating the like count.");
+  }
+});
 
 app.listen(port, () => {
   console.log(`atg is running on port ${port}`);
